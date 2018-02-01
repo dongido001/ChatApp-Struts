7bf561b5787f56c16f24b125ad7bd7e60cfb4f78
Onwuka Gideon <dongidomed@gmail.com>
2018-02-01 15:23:49 +0100

    Update custom.js

--- src/main/webapp/assets/custom.js
+++ src/main/webapp/assets/custom.js
@@ -1,44 +1,20 @@
 $(function() {
-
+	
 	var chatChannel = "";
 
 	$("#startChating").click(function() {
-
+	    	   
 	    if( $("#userName").val() ) { // if user provides username
-
-	    		 fetchAccessToken( $("#userName").val() )
+	    	
+	    	     fetchAccessToken( $("#userName").val() )
+	        	
 	    		 $("#chatName").hide();
 	        	 $(".initiateChat").show();
+				   
 	    	}
 	});
-});
-
-// sending message
-	$("#submitMessage").click( function() {
-
-		var message = $("#message").val();
-		chatChannel.sendMessage(message); // send to Twilio Server
-		$("#message").val("");
-	});
-
-	// listen for messages and append to HTML DOM as we receive them
-    	function appendMessage(data) {
-
-    	    $("#msgItems").append(
-    	            `<div id="chat-item" class="row">
-    	      			<div class="cols-xs-4">
-
-    	                  <p>
-    	                     <p><b>` +data.author+ `</b></p><img src="http://placehold.it/30X30" class="img-circle img-responsive">`
-    	                         +data.body+ `
-    	                  </p>
-
-    	      			</div>
-    	  			</div>`
-    	      );
-    	}
-
-   // get existing channel or create new channel
+	
+	// get existing channel or create new channel
 	function getExistingChannelOrCreateNew(chatClient) {
 	    return chatClient
 	        .getPublicChannelDescriptors()
@@ -47,7 +23,7 @@ $(function() {
 	        	       return paginator.items[0];
 	            }
 	            else {
-
+	            	
 	               chatClient
 	                .createChannel({
 	                   uniqueName: "general",
@@ -67,40 +43,68 @@ $(function() {
 	}
 
 	// initialize the chat
-    	function initailizeChat(token) {
+	function initailizeChat(token) {
+		
+	   Twilio.Chat.Client.create( token )
+	   .then( function(client) {
+		   getExistingChannelOrCreateNew(client)
+		   .then( function(channel) {
+	              return channel.getChannel();
+		   })
+		   .then( function(channel) {
+			   return channel.join(); //join the channel
+		   })
+		   .then( function(channel){
+			   chatChannel = channel; // Assign ...
 
-    	   Twilio.Chat.Client.create( token )
-    	   .then( function(client) {
-    		   getExistingChannelOrCreateNew(client)
-    		   .then( function(channel) {
-    	              return channel.getChannel();
-    		   })
-    		   .then( function(channel) {
-    			   return channel.join(); //join the channel
-    		   })
-    		   .then( function(channel){
-    			   chatChannel = channel;
-    			   // alert("I am done setting up your chat dude!");
-    			   $(".hideForm").show();
-    			   $(".initiateChat").hide();
-    			   channel.on("messageAdded", function(message) {
-                   	                   // append message...
-                   				 appendMessage(message.state);
-                   			   });
-    		   });
-    	   });
-    	}
+			   $(".hideForm").show();
+			   $(".initiateChat").hide();
+		   });
+	   });
+	}
 
-    		// fetch access token
-        	function fetchAccessToken(username) {
-        	    $.post('token', {identity: username, device: 'browser'})
-        	      .done(function(response) {
-        	        console.log('Successfully finished fetch of the Access Token.');
-        	         // Initialize the chat SDK
-        	         initailizeChat(response.token);
+	// fetch access token
+	function fetchAccessToken(username) {
+	    $.post('token', {identity: username, device: 'browser'})
+	      .done(function(response) {
+	        console.log('Successfully finished fetch of the Access Token.');
+	         // Initialize the chat SDK
+	         initailizeChat(response.token);
+	        
+	      })
+	      .fail(function(error) {
+	        console.log('Failed to fetch the Access Token with error: ' + error);
+	      });
+	}
+	
+	// sending message
+	$("#submitMessage").click( function() {
 
-        	      })
-        	      .fail(function(error) {
-        	        console.log('Failed to fetch the Access Token with error: ' + error);
-        	      });
-        	}
+   	 chatChannel.on("messageAdded", function(message) {
+         // append message...
+		 appendMessage(message.state);
+	  });
+   	 
+		var message = $("#message").val();
+		chatChannel.sendMessage(message); // send to Twilio Server
+		$("#message").val("");
+	});
+	
+	
+	// listen for messages and append to HTML DOM as we receive them
+	function appendMessage(data) {
+		
+	    $("#msgItems").append(
+	            `<div id="chat-item" class="row">
+	      			<div class="cols-xs-4">
+	      			
+	                  <p>
+	                     <p><b> ${data.author} </b></p><img src="http://placehold.it/30X30" class="img-circle img-responsive">
+	                         ${data.body}
+	                  </p>
+	                  
+	      			</div>
+	  			</div>`
+	      );
+	}
+});
